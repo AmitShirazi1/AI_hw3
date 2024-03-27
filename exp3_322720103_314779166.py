8c7a418aeb597b9e4d9a64976793473d9a9098cb
Liat Tsipory <liat.tsipory@campus.technion.ac.il>
2024-03-27 20:10:29 +0200

    UCT functions algorithm are done

--- exp3_322720103_314779166.py
+++ exp3_322720103_314779166.py
@@ -2,6 +2,9 @@ IDS = ["322720103", "314779166"]
 from simulator import Simulator
 import random
 import math
+from sample_agent import Agent
+from itertools import product
+from copy import deepcopy
 
 
 class Agent:
@@ -24,14 +27,22 @@ class UCTNode:
     """
     def __init__(self):
         self.parent = None
+        self.action = None
         self.children = []
-        self.successes = 0
+        self.wins = 0
         self.visits = 0
+        self.player_number = 0
     
     def select_using_uct(self):
         if self.visits == 0:
             return float('inf')
-        return self.successes / self.visits + (2 * math.log(self.parent.visits) / self.visits) ** 0.5
+        return self.wins / self.visits + (2 * math.log(self.parent.visits) / self.visits) ** 0.5
+    
+    def update_node(self, result):
+        self.visits += 1
+        is_winner = result['player '+str(self.player_number)] > result['player '+str(3 - self.player_number)]
+        if is_winner > 0:
+            self.wins += 1
 
 
 class UCTTree:
@@ -48,73 +59,93 @@ class UCTAgent:
         self.player_number = player_number
         self.my_ships = []
         self.simulator = Simulator(initial_state)
+        self.agent = Agent(initial_state, player_number)
         for ship_name, ship in initial_state['pirate_ships'].items():
             if ship['player'] == player_number:
                 self.my_ships.append(ship_name)
 
-    # def selection(self, UCT_tree):
-    def selection(self, node):
-        curr_node = node
-        while len(curr_node.children) > 0:
-            curr_node = max(curr_node.children, key=lambda x: x.select_using_uct())
+    def selection(self, root):
+        curr_node = root
+        while curr_node.children:
+            max_uct_value = float('-inf')
+            max_uct_children = []
+            for child in curr_node.children:
+                uct_value = child.select_using_uct()
+                if uct_value > max_uct_value:
+                    max_uct_value = uct_value
+                    max_uct_children = [child]
+                elif uct_value == max_uct_value:
+                    max_uct_children.append(child)
+            curr_node = random.choice(max_uct_children)
+            # TODO: Maybe keep expanding (building) tree if init is not enough
         return curr_node
+    
 
-    def is_deposit_action_legal(deposit_action, player):
-        pirate_name = deposit_action[1]
-        treasure_name = deposit_action[2]
-        # check same position
-        if player != self.state['pirate_ships'][pirate_name]['player']:
-            return False
-        if self.state["pirate_ships"][pirate_name]["location"] != self.base_location:
-            return False
-        if self.state['treasures'][treasure_name]['location'] != pirate_name:
-            return False
-        return True
-
-    def is_plunder_action_legal(plunder_action, player):
-        pirate_1_name = plunder_action[1]
-        pirate_2_name = plunder_action[2]
-        if player != self.state["pirate_ships"][pirate_1_name]["player"]:
-            return False
-        if self.state["pirate_ships"][pirate_1_name]["location"] != self.state["pirate_ships"][pirate_2_name]["location"]:
-            return False
-        return True
-
-    def get_legal_actions(self, node):
-        legal_actions = []
-        state = self.simulator.get_state()
+    def get_legal_actions(self, state):
+        actions = {}
+        collected_treasures = []
         for ship in self.my_ships:
+            actions[ship] = set()
             neighboring_tiles = self.simulator.neighbors(state["pirate_ships"][ship]["location"])
             for tile in neighboring_tiles:
-                legal_actions.append(("sail", ship, tile))
+                actions[ship].add(("sail", ship, tile))
             if state["pirate_ships"][ship]["capacity"] > 0:
                 for treasure in state["treasures"].keys():
-                    if state["pirate_ships"][ship]["location"] in self.simulator.neighbors(state["treasures"][treasure]["location"]):
-                        legal_actions.append(("collect", ship, treasure))
-                for treasure in state["treasures"].keys():
-                    if state["pirate_ships"][ship]["location"] == state["base"] and \
-                            state["treasures"][treasure]["location"] == ship:
-                        legal_actions.append(("deposit", ship, treasure))
-                for enemy_ship_name in state["pirate_ships"].keys():
-                    if state["pirate_ships"][ship]["location"] == state["pirate_ships"][enemy_ship_name]["location"] and \
-                            self.player_number != state["pirate_ships"][enemy_ship_name]["player"]:
-                        legal_actions.append(("plunder", ship, enemy_ship_name))
-                legal_actions.append(("wait", ship))
-        return legal_actions
+                    if state["pirate_ships"][ship]["location"] in self.simulator.neighbors(state["treasures"][treasure]["location"])\
+                        and treasure not in collected_treasures:
+                        actions[ship].add(("collect", ship, treasure))
+                        collected_treasures.append(treasure)
+            for treasure in state["treasures"].keys():
+                if (state["pirate_ships"][ship]["location"] == state["base"]
+                        and state["treasures"][treasure]["location"] == ship):
+                    actions[ship].add(("deposit", ship, treasure))
+            for enemy_ship_name in state["pirate_ships"].keys():
+                if (state["pirate_ships"][ship]["location"] == state["pirate_ships"][enemy_ship_name]["location"] and
+                        self.player_number != state["pirate_ships"][enemy_ship_name]["player"]):
+                    actions[ship].add(("plunder", ship, enemy_ship_name))
+            actions[ship].add(("wait", ship))
+        return list(product(*actions.values()))
 
+        
     # def expansion(self, UCT_tree, parent_node):
-    def expansion(self, node, parent_node):
-        for action in self.get_legal_actions(node):
+    def expansion(self, state, parent_node):
+        for action in self.get_legal_actions(state):
             # TODO: create a get_actions function, and then call act.
             new_node = UCTNode()
             new_node.parent = parent_node
+            new_node.action = action
             parent_node.children.append(new_node)
+        return random.choice(parent_node.children)
+    
+    # before calling simulation we will make a deep copy of the simulator so we won't change the original simulator
 
-    def simulation(self):
-        raise NotImplementedError
+    def simulation(self, node, turns_to_go, player, simulator):
+        if turns_to_go == 0:
+            return simulator.get_score()  # Score is a dictionary with player 1 and player 2 scores.
+        if node.children:
+            child_node = random.choice(node.children)
+        else :
+            child_node = self.expansion(node)
+        simulator.act(child_node.action, player)
+        return self.simulation(child_node, (turns_to_go - 1), (3 - player), simulator)
 
-    def backpropagation(self, simulation_result):
-        raise NotImplementedError
+
+    def backpropagation(self, node, simulation_result):
+        while node is not None:
+            node.update_node(simulation_result)
+            node = node.parent
+
 
     def act(self, state):
-        raise NotImplementedError
+        root = UCTNode()
+        for _ in range(100):
+            node = self.selection(root)
+            if self.simulator.turns_to_go == 0:
+                self.backpropagate(node, self.simulator.get_score())
+            else:
+                self.expansion(state, node)
+                simulation_result = self.simulation(node, self.simulator.turns_to_go, self.player_number, deepcopy(self.simulator))
+                self.backpropagation(node, simulation_result)
+        child =  max(root.children, key=lambda child: child.wins / child.visits)  # No exploration
+        return child.action
+
